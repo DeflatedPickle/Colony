dc4c421d87ae77489912f8c805f6b7d4898d2ea1
DeflatedPickle <DeflatedPickle@gmail.com>
2017-09-28 07:55:41 +0100

    Updated a few functions

--- colony/sound.py
+++ colony/sound.py
@@ -20,16 +20,21 @@ class Sound(object):
         self._linger = linger
 
         self._sound_object = None
+        self.dissipate_number = 0
+
+    def draw_sound(self, x0, y0, x1, y1):
+        self._sound_object = self._canvas.create_oval(self._producer.location["x"] - x0,
+                                                      self._producer.location["y"] - y0,
+                                                      self._producer.location["x"] + x1,
+                                                      self._producer.location["y"] + y1,
+                                                      tags="sound")
 
     def create_given_sound(self, sound):
         """Creates a sound at the given sound level."""
-        self._sound_object = self._canvas.create_oval(self._producer.location["x"] - sound,
-                                                      self._producer.location["y"] - sound,
-                                                      self._producer.location["x"] + sound,
-                                                      self._producer.location["y"] + sound,
-                                                      tags="sound")
+        self.draw_sound(sound, sound, sound, sound)
 
-        self._canvas.master.after(self._linger, self.destroy_sound)
+        # self._canvas.master.after(self._linger, self.destroy_sound)
+        self._canvas.master.after(self._linger // 2, self.dissipate_sound)
 
     def create_sound(self):
         """Creates a sound at the set sound level."""
@@ -40,7 +45,8 @@ class Sound(object):
         self.create_given_sound(randint(self._sound - randint(0, 5), self._sound + randint(0, 5)))
 
     def resize_sound(self, x0, y0, x1, y1):
-        self._canvas.master.coords(self._sound_object, self._producer.location["x"] - x0, self._producer.location["y"] - y0, self._producer.location["x"] + x1, self._producer.location["y"] + y1)
+        self.destroy_sound()
+        self.draw_sound(x0, y0, x1, y1)
 
     def resize_sound_by(self, amount):
         self.resize_sound(amount, amount, amount, amount)
@@ -50,5 +56,12 @@ class Sound(object):
         self._canvas.delete(self._sound_object)
 
     def dissipate_sound(self):
-        """Slowly makes the sound quieter till it's gone."""
-        pass
+        """Slowly makes the sound get quieter until it's gone."""
+        self.dissipate_number += 1
+
+        if self.dissipate_number <= 10:
+            self.resize_sound_by(self.dissipate_number * -1)
+            self._canvas.master.after(self._linger // 2, self.dissipate_sound)
+
+        else:
+            self.destroy_sound()
